9f2cb5e5f464d22810b9310bc454d1dd7a7f60d6
Nita W <nitawasniowski@gmail.com>
2023-11-22 02:03:22 +0100

    gpxgenerator edited

--- app/src/main/java/and06/geonotes/GatherActivity.kt
+++ app/src/main/java/and06/geonotes/GatherActivity.kt
@@ -9,6 +9,7 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -154,6 +155,10 @@ class GatherActivity : AppCompatActivity() {
 
             R.id.menu_notiz_loeschen -> {
                 notizLoeschen()
+            }
+
+            R.id.menu_projekt_versenden -> {
+                projektVersenden()
             }
         }
         return super.onOptionsItemSelected(item)
@@ -701,6 +706,45 @@ class GatherActivity : AppCompatActivity() {
         outState.putParcelable(AKTUELLES_PROJEKT, aktuellesProjekt)
         if (aktuelleNotiz != null)
             outState.putParcelable(AKTUELLE_NOTIZ, aktuelleNotiz)
+    }
+
+    fun projektVersenden() {
+        if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            Toast.makeText(
+                this, "External Storage nicht verfügbar",
+                Toast.LENGTH_LONG
+            ).show()
+            return
+        }
+        val database = GeoNotesDatabase.getInstance(this)
+        GlobalScope.launch {
+            val notizen = database.notizenDao().getNotizen(aktuellesProjekt.id)
+            if (notizen.isNullOrEmpty()) {
+                Toast.makeText(
+                    this@GatherActivity, "keine Notizen vorhanden ",
+                    Toast.LENGTH_LONG
+                ).show()
+                return@launch
+            }
+            val generator = GpxGenerator()
+            val uri = generator.createGpxFile(
+                this@GatherActivity,
+                notizen, aktuellesProjekt.getDescription()
+            )
+            with(Intent(Intent.ACTION_SEND)) {
+                type = "text/xml"
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("nitawasniowski@gmail.com"))
+                putExtra(Intent.EXTRA_SUBJECT, aktuellesProjekt.getDescription())
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    "In the attachment is a generated xml file in gpx Format"
+                )
+                putExtra(Intent.EXTRA_STREAM, uri)
+                startActivity(
+                    Intent.createChooser(this, "Mail versenden ")
+                )
+            }
+        }
     }
 
 }
--- app/src/main/java/and06/geonotes/GpxGenerator.kt
+++ app/src/main/java/and06/geonotes/GpxGenerator.kt
@@ -2,16 +2,30 @@ package and06.geonotes;
 
 import android.content.Context
 import android.net.Uri
+import android.os.Environment
 import android.util.Log
+import androidx.core.content.FileProvider
 import org.w3c.dom.Document
+import java.io.File
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.parsers.ParserConfigurationException
+import javax.xml.transform.TransformerConfigurationException
+import javax.xml.transform.TransformerException
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
 
 class GpxGenerator {
-    fun createGpxFile(context: Context, notizen: List<Notiz>):
-            Uri? {
+    fun createGpxFile(context: Context,
+                      notizen: List<Notiz>,
+                      projektName: String): Uri? {
         val document = createGpxDocument()
-        return null // provisorisch!
+        return document?.let {
+            createRootElement(it)
+            createMetaData(it, projektName)
+            appendTrackPoints(it, notizen)
+            serialize(context, it)
+        }
     }
 
     private fun createGpxDocument(): Document? {
@@ -24,5 +38,66 @@ class GpxGenerator {
             null
         }
         return gpxDocument
+    }
+
+    private fun createRootElement(document: Document) {
+        with(document.createElement("gpx")) {
+            setAttribute("xmlns", "https://www.topografix.com/GPX/1/1/")
+            setAttribute("xmlns:xsi", "https://www.w3.org/2001/ XMLSchema-instance")
+            setAttribute(
+                "xsi:schemaLocation",
+                "https:// www.topografix.com/GPX/1/1/gpx.xsd https:// " +
+                        "www.topografix.com/GPX/1/1/gpx.xsd"
+            )
+            setAttribute("version", "1.1")
+            setAttribute("creator", "GeoNotes")
+            document.appendChild(this)
+        }
+    }
+
+    private fun appendTrackPoints(document: Document, notizen: List<Notiz>) {
+        val trk = document.createElement("trk")
+        document.documentElement.appendChild(trk)
+        val trkseg = document.createElement("trkseg")
+        trk.appendChild(trkseg)
+        notizen.forEach {
+            with(document.createElement("trkpt")) {
+                setAttribute("lat", it.latitude.toString())
+                setAttribute("lon", it.longitude.toString())
+                val name = document.createElement("name")
+                name.appendChild(document.createTextNode(it.thema))
+                appendChild(name)
+                val desc = document.createElement("desc")
+                desc.appendChild(document.createTextNode(it.notiz))
+                appendChild(desc)
+                trkseg.appendChild(this)
+            }
+        }
+    }
+
+    private fun createMetaData(document: Document, projektName: String) {
+        val metadata = document.createElement("medatadata")
+        document.documentElement.appendChild(metadata)
+        val name = document.createElement("desc")
+        name.appendChild(document.createTextNode(projektName))
+        metadata.appendChild(name)
+    }
+
+    private fun serialize(context: Context, document: Document): Uri? {
+        val documentsDirectory = context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+        val destFile = File(documentsDirectory, "geonotes.gpx")
+        val factory = TransformerFactory.newInstance()
+        val uri = try {
+            val transformer = factory.newTransformer()
+            transformer.transform(DOMSource(document.documentElement), StreamResult(destFile))
+            FileProvider.getUriForFile(context, "geonotes.fileprovider", destFile)
+        } catch (ex: TransformerConfigurationException) {
+            Log.d(javaClass.simpleName, ex.toString())
+            null
+        } catch (ex: TransformerException) {
+            Log.d(javaClass.simpleName, ex.toString())
+            null
+        }
+        return uri
     }
 }
